0618cd5f7788991719172b4db9744e2af5f48c9f
Anish Karandikar <anishkny@gmail.com>
2018-05-20 22:44:47 -0400

    feat(MainPage): Add global feed
    Add "Fork me on GitHub" footer

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -12,6 +12,32 @@
           </li> -->
         </ul>
       </div>
+      <div>
+        <div v-for="article in articles" class="article-preview">
+          <div class="article-meta">
+            <a class="" :href="'profile/' + article.author.username">
+              <img :src="article.author.image">
+            </a>
+            <div class="info">
+              <a class="author" :href="'profile/' + article.author.username">
+                {{article.author.username}}
+              </a>
+              <span class="date">{{article.updatedAtDisplay}}</span>
+            </div>
+            <div class="pull-xs-right">
+              <button class="btn btn-sm btn-outline-primary">
+                <i class="ion-heart"></i>&nbsp;{{article.favoritesCount}}
+              </button>
+            </div>
+          </div>
+          <a class="preview-link" :href="'article/' + article.slug">
+            <h1>{{article.title}}</h1>
+            <p>{{article.description}}</p>
+            <span>Read more...</span>
+            <ul class="tag-list"></ul>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="col-md-3">
       <div class="sidebar">
@@ -27,18 +53,23 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
 
   data() {
     return {
       tags: [],
+      articles: [],
     }
   },
 
   async created() {
     const tags = (await axios.get('https://conduit.productionready.io/api/tags')).data.tags;
     this.tags = tags;
+    const articles = (await axios.get('https://conduit.productionready.io/api/articles?limit=10&offset=0')).data.articles;
+    articles.forEach(a => a.updatedAtDisplay = moment(a.updatedAt).format('ddd MMM D YYYY'));
+    this.articles = articles;
   },
 
 }
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,9 @@
     <banner></banner>
     <main-page></main-page>
   </div>
+  <a id="forkOnGH" href="https://github.com/anishkny/realworld-nuxt" target="_top" style="">
+  <i class="ion-social-github"></i>&nbsp;&nbsp;Fork on GitHub</a>
+
 </div>
 </template>
 
@@ -21,3 +24,20 @@ export default {
   },
 }
 </script>
+
+
+<style scoped>
+#forkOnGH {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: linear-gradient(#485563, #29323c);
+  text-align: center;
+  padding: 15px;
+  box-shadow: 0 5px 5px 5px rgba(0, 0, 0, 0.4);
+  z-index: 999;
+  color: #fff;
+  font-size: 1.5rem;
+  display: block
+}
+</style>
